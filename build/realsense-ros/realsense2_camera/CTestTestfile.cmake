dbc4354f630bd3a4fd3eaf25d395c4a5f1807a71
OU, TING-WEI <87459096+ouotingwei@users.noreply.github.com>
2024-02-29 15:42:06 +0800

    Delete build directory

--- build/realsense-ros/realsense2_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wei/visual_based_pose_estimator/src/realsense-ros/realsense2_camera
-# Build directory: /home/wei/visual_based_pose_estimator/build/realsense-ros/realsense2_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
